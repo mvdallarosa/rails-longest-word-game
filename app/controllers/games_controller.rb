f66b275feeb9e2497477108eec4725f024d0b4cb
Maria Vittoria Dalla Rosa Prati <mvdallarosa@gmail.com>
2020-10-19 17:00:52 +0200

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,6 @@
 require 'open-uri'
 require 'json'
 
-
 class GamesController < ApplicationController
   def new
     @grid = []
@@ -14,14 +13,28 @@ class GamesController < ApplicationController
     grid_string = @grid.join(', ')
     @attempt_array = @attempt.upcase.split('')
     @check = @attempt_array.all? { |letter| @grid.count(letter) >= @attempt_array.count(letter) && @grid.include?(letter) }
-    lewagon_dict = open("https://wagon-dictionary.herokuapp.com/#{@attempt}").read
+    @result = result(@attempt)
+  end
+
+  def result(attempt)
+    lewagon_dict = open("https://wagon-dictionary.herokuapp.com/#{attempt}").read
     word = JSON.parse(lewagon_dict)
     if word['found'] == false
-      @result = "Sorry but #{@attempt.upcase} does not seem to be a valid English word..."
+      result = "Sorry but #{attempt.upcase} does not seem to be a valid English word..."
     elsif @check && word['found']
-      @result = "Congratulations! #{@attempt.upcase} is a valid English word!"
+      result = "Congratulations! #{attempt.upcase} is a valid English word! Your score is #{final(word['length'])}"
     else
-      @result = "Sorry but #{@attempt.upcase} can't be built out #{grid_string}"
+      result = "Sorry but #{attempt.upcase} can't be built out #{grid_string}"
     end
+    result
+  end
+
+  def final(new_score)
+    if session.has_key?(:score)
+      session[:score] += new_score
+    else
+      session[:score] = new_score
+    end
+    session[:score]
   end
 end
